8cfa51c84718246a7cd903c1863c38dbeea3eaeb
Michael Bednarik <michi.bednarik@gmail.com>
2026-03-25 22:29:47 +0100

    basic json read/write

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+
+
+def write(filename: str, exercise: str, weight: int, reps: int) -> None:
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    data[exercise] = {
+        "weight": weight,
+        "reps": reps,
+    }
+
+    
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+def read(filename: str, exercise: str | None = None) -> dict:
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    if exercise == None:
+        return data
+    else:
+        output = data.get(exercise)
+
+        if output is None:
+            return {}
+        else:
+            return output
+
+
+if __name__ == "__main__":
+    n = read("data.json", "lat_pull")
+    print(n)
+        
