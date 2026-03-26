2885cc0e1799380bfd49593e429f0a6d26eb0c7f
SchulLaptop <michi.bednarik@gmail.com>
2026-03-26 11:26:23 +0100

    basic layout

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from PyQt6.QtCore import Qt
-
 from api import read, write
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QHBoxLayout
 import sys
 
 
@@ -9,6 +7,24 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("gym-tracker")
+        
+        rows = QVBoxLayout()
+        
+        for i in range(3):
+            columns = QHBoxLayout()
+            for j in range(3):
+                columns.addWidget(QLineEdit())
+            wgt = QWidget()
+            wgt.setLayout(columns)
+            rows.addWidget(wgt)
+        
+        
+        
+        container = QWidget()
+        container.setLayout(rows)
+        self.setCentralWidget(container)
+        
+        
 
 
 if __name__ == "__main__":
