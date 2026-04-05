ce2a4d9df7239dbb817aecbd2dcae892d4cb1558
Michael Bednarik <michi.bednarik@gmail.com>
2026-04-06 00:56:29 +0200

    change/add attributes with post requests

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
 
@@ -10,6 +10,23 @@ def get_attr(attr):
     with open("data.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return str(data[attr])
+
+@app.route("/write_attr", methods=["POST"])
+def write_attr():
+    body = request.get_json()
+    text = body["text"]
+    attr = body["attr"]
+    
+    with open("data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    data[attr] = text
+    
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    
+    return jsonify({"success": True })
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
