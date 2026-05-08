c64ef995cd52c31008dac8b0623f1a54ec765195
Michael Bednarik <michi.bednarik@gmail.com>
2026-05-08 17:49:07 +0200

    finished tutorial and messed around a little

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const stars = [135850, 52122, 148825, 16939, 9764]
 const frameworks = ["React", "Angular", "Vue", "Hyperapp", "Omi"]
 
+let stars2 = []
+let stars3 = []
+
+stars.forEach(star => {
+    stars2.push(star/2)
+})
+
+stars.forEach(star => {
+    stars3.push(star/5 + 100000)
+})
+
 const ctx = document.getElementById("myChart")
 
 const myChart = new Chart(ctx, {
-    type: "pie",
+    type: "line",
     data: {
         labels: frameworks,
         datasets: [
             {
                 label: "Popular JavaScript Frameworks",
                 data: stars,
+                tension: 0.4,
+                showLine: false,
             },
+            {
+                label: "frameworks2",
+                data: stars2,
+                tension: 0.4,
+
+            },
+            {
+                label: "frameworks3",
+                data: stars3,
+                tension: 0.4,
+            }
         ],
     }
 })
